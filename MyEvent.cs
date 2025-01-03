6456d1371af4f27cc82c91812e9b8d225dac55f6
Blaz <boblak2@gmail.com>
2025-01-03 18:19:25 +0100

    Added stuff

---
+++ MyEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppGitty
+{
+    internal class MyEvent
+    {
+    }
+
+    public class KebabEvent
+    {
+
+    }
+
+    public class BurekEvent
+    {
+
+    }
+}
